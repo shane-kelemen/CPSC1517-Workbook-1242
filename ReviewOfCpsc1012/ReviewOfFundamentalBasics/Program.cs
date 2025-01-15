93f215d5ccd827550b7f79ff5430b7db5f867c84
shane-kelemen <43184468+shane-kelemen@users.noreply.github.com>
2025-01-14 21:11:42 -0700

    Demo Console App Upload Solo

--- ReviewOfCpsc1012/ReviewOfFundamentalBasics/Program.cs
+++ ReviewOfCpsc1012/ReviewOfFundamentalBasics/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is new code to facilitate GitHub upload demo");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
    
